e0b49be62d9aadb8e7d259f3ba791ac67254f80f
ardszsantos <lokgamer01@gmail.com>
2025-04-12 15:26:45 -0300

    feat: added the correct expected returns for update and delete pet

--- src/repositories/interfaces/InterfacePetRepository.ts
+++ src/repositories/interfaces/InterfacePetRepository.ts
@@ -3,6 +3,7 @@ import PetEntity from "../../entities/PetEntity";
 export default interface InterfacePetRepository {
     criaPet(pet: PetEntity):void;
     listaPet(): Array<PetEntity> | Promise<PetEntity[]> ;
-    atualizaPet(id:number, pet: PetEntity):void;
-    deletaPet(id:number, pet: PetEntity):void;
+    atualizaPet(id:number, pet: PetEntity): Promise<{success: boolean; message?: string}> | void;
+    deletaPet(id:number): Promise <{success: boolean; message?: string}>
+    
 }
